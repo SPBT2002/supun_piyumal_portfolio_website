0ac5178a4b98054d757e21569f1954f335d2a7e7
Supun Piyumal <supunthennakoon27@gmail.com>
2025-10-18 23:01:51 +0530

    Home Page

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["Web Developer", "Frontend Developer", "Backend Developer"],
+    typeSpeed: 95,
+    backSpeed: 95,
+    backDelay: 1000,
+    loop: true
+});
